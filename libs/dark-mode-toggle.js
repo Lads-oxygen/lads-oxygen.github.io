b5d2b58fd2c752beb335398f4676e5ce7a8c01a8
Lads-oxygen <67551144+Lads-oxygen@users.noreply.github.com>
2024-09-16 19:10:35 +0000

    Deploying to gh-pages from  @ de3b0de727ca720f71fda3c909bd360eea6f02a7 🚀

---
+++ libs/dark-mode-toggle.js
@@ -0,0 +1,32 @@
+if (localStorage.getItem('darkMode') === 'enabled') {
+    document.documentElement.classList.add('dark-mode');
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const darkModeCheckbox = document.getElementById('dark-mode-checkbox');
+    const darkModeClass = 'dark-mode';
+    const moonSvg = document.getElementById('moon-svg');
+    const sunSvg = document.getElementById('sun-svg');
+
+    // Apply dark mode class immediately if preference is set
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.documentElement.classList.add(darkModeClass);
+        darkModeCheckbox.checked = true;
+        moonSvg.style.display = 'block';
+        sunSvg.style.display = 'none';
+    }
+
+    darkModeCheckbox.addEventListener('change', () => {
+        if (darkModeCheckbox.checked) {
+            document.documentElement.classList.add(darkModeClass);
+            localStorage.setItem('darkMode', 'enabled');
+            moonSvg.style.display = 'block';
+            sunSvg.style.display = 'none';
+        } else {
+            document.documentElement.classList.remove(darkModeClass);
+            localStorage.setItem('darkMode', 'disabled');
+            moonSvg.style.display = 'none';
+            sunSvg.style.display = 'block';
+        }
+    });
+});
